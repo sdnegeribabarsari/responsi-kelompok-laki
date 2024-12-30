4029ba0dee30abff9eafc13d4362ecc81a0f897a
sdnegeribabarsari <arifagung1806@gmail.com>
2024-12-30 08:58:25 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,49 @@
 import streamlit as st
+import pickle
+import numpy as np
 import pandas as pd
-import math
-from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Load Scaler, LSTM model, dan SVM classifier
+with open('model/scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+with open('model/lstm_model.pkl', 'rb') as file:
+    lstm_model = pickle.load(file)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+with open('model/svm_classifier.pkl', 'rb') as file:
+    svm_classifier = pickle.load(file)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Judul Streamlit
+st.title('Prediksi apakah Jamur ini bisa dimakan? 🍲😋⁉')
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Input fitur numerik
+input_features = []
+feature_names = ['pm10', 'pm25', 'so2', 'co', 'o3', 'no2', 'max']
+for feature in feature_names:
+    value = st.number_input(f'Masukkan nilai untuk {feature}', value=0, step=1)
+    input_features.append(value)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+df = pd.DataFrame([input_features], columns=feature_names)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+def prediction(df):
+    # Skala fitur numerik menggunakan Scaler yang dimuat
+    scaled_data = scaler.transform(df)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    # Bentuk ulang input untuk LSTM
+    input_data = scaled_data.reshape((scaled_data.shape[0], 1, scaled_data.shape[1]))
 
-    return gdp_df
+    # Ekstrak fitur menggunakan model LSTM
+    features = lstm_model.predict(input_data)
 
-gdp_df = get_gdp_data()
+    # Lakukan prediksi menggunakan model SVM
+    prediction = svm_classifier.predict(features)
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+    if prediction:
+        prediction = 'beracun ☠, jangan ya dek ya 🙅'
+    else:
+        prediction = 'bisa dimakan 🍲😋, gasin aja bang👍'
+    return prediction
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+if st.button("Prediksi"):
+    prediction_result = prediction(df)
+    st.write('Jamur ini merupakan jamur yang', prediction_result)
